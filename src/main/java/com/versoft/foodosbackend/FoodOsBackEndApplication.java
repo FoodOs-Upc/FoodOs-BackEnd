5ea7d54ea09633db9a5554d2ee755cc3f9a52967
Brays83 <102981505+Brays83@users.noreply.github.com>
2024-05-22 03:13:20 -0500

    chore(main):
    initial commit

---
+++ src/main/java/com/versoft/foodosbackend/FoodOsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.versoft.foodosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOsBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodOsBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/versoft/foodosbackend/FoodOsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.versoft.foodosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOsBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
