aba2fca6658a71d6cfead12e0dd29899a1764eda
Alessandro  Vega <83603934+Alessandrovega@users.noreply.github.com>
2024-06-05 14:25:07 -0500

    TaskResource
    Completed TaskResource

---
+++ src/main/java/com/versoft/foodosbackend/Task/Interface/Rest/Resource/UpdateTaskResource.java
@@ -0,0 +1,7 @@
+package com.versoft.foodosbackend.Task.Interface.Rest.Resource;
+
+public record UpdateTaskResource (String title,
+                                  String description,
+                                  String date,
+                                  String memberasignado) {
+}
