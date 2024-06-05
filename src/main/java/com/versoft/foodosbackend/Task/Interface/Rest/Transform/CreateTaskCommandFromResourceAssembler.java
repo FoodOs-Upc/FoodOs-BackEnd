563a0b45dde26d5a76865f75509637785407308a
Alessandro  Vega <83603934+Alessandrovega@users.noreply.github.com>
2024-06-05 14:33:10 -0500

    CreateTaskCommandFromResourceAssembler
    Completed

--- src/main/java/com/versoft/foodosbackend/Task/Interface/Rest/Transform/CreateTaskCommandFromResourceAssembler.java
+++ src/main/java/com/versoft/foodosbackend/Task/Interface/Rest/Transform/CreateTaskCommandFromResourceAssembler.java
@@ -1,4 +1,10 @@
 package com.versoft.foodosbackend.Task.Interface.Rest.Transform;
 
+import com.versoft.foodosbackend.Task.Domain.Model.Commands.CreateTaskCommand;
+import com.versoft.foodosbackend.Task.Interface.Rest.Resource.CreateTaskResource;
+
 public class CreateTaskCommandFromResourceAssembler {
+    public static CreateTaskCommand toCommandFromResource(CreateTaskResource resource){
+        return new CreateTaskCommand(resource.title(), resource.description(), resource.date(), resource.memberasignado());
+    }
 }
